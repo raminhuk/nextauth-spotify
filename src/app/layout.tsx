9a187420044c5b90a53bf4b2d4eed26265657f81
Fabio J Raminhuk <fabioraminhuk@gmail.com>
2023-08-31 19:56:14 -0300

    style: 💄 ajustando style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <AuthProvider>
           <Navbar />
-          <main className="flex justify-center items-start p-6 min-h-screen">
+          <main className="flex justify-center items-start p-6">
             {children}
           </main>
         </AuthProvider>
